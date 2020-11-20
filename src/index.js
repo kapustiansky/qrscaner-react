f065af207f5c5f0632b9c4f99a00f67b75470591
Bot1101 <kapustianskyov@gmail.com>
2020-11-20 18:47:07 +0200

    hash add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//import { HashRouter, Route } from "react-router-dom";
 
 import LocalStor from './components/localstor-item';
 
@@ -10,11 +10,11 @@ import './index.css';
 const App = () => {
 
 return	(
-	<HashRouter  basename="/qrscaner-react">
+	<Router  basename="/qrscaner-react">
 	<section className="container my-5 px-0 z-depth-1 p-5 my-md-5 text-center main-section">
 			<Route path="/" component={LocalStor} />
 	</section>
-	</HashRouter>
+	</Router>
 	);
 }
 
